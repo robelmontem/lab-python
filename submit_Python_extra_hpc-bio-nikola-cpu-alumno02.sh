e98b1fc112d0ac150896d5aa7f2a97599efa9390
robelmontem <roberto.belmontem@um.es>
2025-12-14 15:13:55 +0100

    Ejercicio extra

--- submit_Python_extra_hpc-bio-nikola-cpu-alumno02.sh
+++ submit_Python_extra_hpc-bio-nikola-cpu-alumno02.sh
@@ -2,13 +2,13 @@
 
 #SBATCH -p hpc-bio-nikola-cpu
 #SBATCH --chdir=/home/alumno02/
-#SBATCH -J lab4-3.3
-#SBATCH --cpus-per-task=1
+#SBATCH -J lab4-extra
+#SBATCH --cpus-per-task=4
 #SBATCH --ntasks=1
 
 module load anaconda/2025.06
 
-$
-ipython reduc-operation-alumno02.ipynb $1
+$NUMBA_NUM_THREADS=$SLURM_CPUS_PER_TASK
+ipython pi.ipynb
 
 module unload anaconda/2025.06
